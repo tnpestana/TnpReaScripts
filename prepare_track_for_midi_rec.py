79e0d125516ce4d7962612760765f85a08690f4c
Tiago Pestana <tnevespestana@gmail.com>
2019-04-08 23:11:42 +0100

    uploaded first script - preparing track for midi recording

---
+++ prepare_track_for_midi_rec.py
@@ -0,0 +1,4 @@
+track = RPR_GetSelectedTrack(0, 0)
+RPR_SetMediaTrackInfo_Value(track, "I_RECARM", 1)
+RPR_SetMediaTrackInfo_Value(track, "I_RECMON", 1)
+RPR_SetMediaTrackInfo_Value(track, "I_RECINPUT", 6112)
