a5ec0d1787653abccd2c118e57b1d53488fcb103
Tiago Pestana <tnevespestana@gmail.com>
2019-04-13 19:07:31 +0100

    first lua scripts - arm rec all selected tracks

---
+++ Lua/SwitchRecStatusAllSelectedTracks.lua
@@ -0,0 +1,9 @@
+selected_tracks = reaper.CountSelectedTracks(0)
+for i = 0, selected_tracks - 1 do
+	track = reaper.GetSelectedTrack(0, i)
+	if reaper.GetMediaTrackInfo_Value(track, "I_RECARM") == 0 then
+		reaper.SetMediaTrackInfo_Value(track, "I_RECARM", 1)
+	else
+		reaper.SetMediaTrackInfo_Value(track, "I_RECARM", 0)
+	end
+end
